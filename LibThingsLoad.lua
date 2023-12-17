bdf41c1d7992f419ce7b35bf2e06dc775be82e46
sfmict <sfmict@gmail.com>
2023-12-17 16:53:36 +0000

    Now can add new IDs to promise

--- LibThingsLoad.lua
+++ LibThingsLoad.lua
@@ -2,12 +2,14 @@
 ---------------------------------------------------------------
 -- LibThingsLoad - Library for load quests, items and spells --
 ---------------------------------------------------------------
-local MAJOR_VERSION, MINOR_VERSION = "LibThingsLoad-1.0", 4
+local MAJOR_VERSION, MINOR_VERSION = "LibThingsLoad-1.0", 5
 local lib, oldminor = LibStub:NewLibrary(MAJOR_VERSION, MINOR_VERSION)
 if not lib then return end
 
 
 local type, next, xpcall, setmetatable, CallErrorHandler, C_Item, C_Spell = type, next, xpcall, setmetatable, CallErrorHandler, C_Item, C_Spell
+local DoesItemExistByID, IsItemDataCachedByID, GetItemInfo, ITEM_QUALITY_COLORS, GetDetailedItemLevelInfo, GetItemInfoInstant = C_Item.DoesItemExistByID, C_Item.IsItemDataCachedByID, GetItemInfo, ITEM_QUALITY_COLORS, GetDetailedItemLevelInfo, GetItemInfoInstant
+local DoesSpellExist, IsSpellDataCached, GetSpellInfo, GetSpellSubtext, GetSpellTexture, GetSpellDescription = C_Spell.DoesSpellExist, C_Spell.IsSpellDataCached, GetSpellInfo, GetSpellSubtext, GetSpellTexture, GetSpellDescription
 
 
 if not lib._listener then
@@ -38,17 +40,17 @@ local listener = lib._listener
 
 
 function listener:QUEST_DATA_LOAD_RESULT(...)
-	self:FireCallbacks(self.types.quest, ...)
+	self:fireCallbacks(self.types.quest, ...)
 end
 
 
 function listener:ITEM_DATA_LOAD_RESULT(...)
-	self:FireCallbacks(self.types.item, ...)
+	self:fireCallbacks(self.types.item, ...)
 end
 
 
 function listener:SPELL_DATA_LOAD_RESULT(...)
-	self:FireCallbacks(self.types.spell, ...)
+	self:fireCallbacks(self.types.spell, ...)
 end
 
 
@@ -62,31 +64,27 @@ function listener:checkThen(p)
 end
 
 
-function listener:FireCallbacks(loadType, id, success)
+function listener:firePromiseCallbacks(loadType, p, id, success)
+	local pt = p[loadType]
+
+	if pt[id] == -1 then
+		pt[id] = success
+		pt.count = pt.count + 1
+
+		if success then if p._thenForAll then p:_thenForAll(id, loadType) end
+		elseif p._fail then p:_fail(id, loadType) end
+		if pt.count == pt.total and p._then and self:checkThen(p) then p:_then() end
+	end
+end
+
+
+function listener:fireCallbacks(loadType, id, success)
 	local ps = self[loadType][id]
 	if ps then
 		self[loadType][id] = nil
 
 		for i = 1, #ps do
-			local p = ps[i]
-			local pt = p[loadType]
-
-			if pt[id] == -1 then
-				pt[id] = success
-				pt.count = pt.count + 1
-
-				if success then
-					if p._thenForAll then
-						xpcall(p._thenForAll, CallErrorHandler, p, id, loadType)
-					end
-				elseif p._fail then
-					xpcall(p._fail, CallErrorHandler, p, id, loadType)
-				end
-
-				if pt.count == pt.total and p._then and self:checkThen(p) then
-					xpcall(p._then, CallErrorHandler, p)
-				end
-			end
+			xpcall(self.firePromiseCallbacks, CallErrorHandler, self, loadType, ps[i], id, success)
 		end
 	end
 end
@@ -100,76 +98,24 @@ function listener:loadID(loadType, id, p)
 end
 
 
-function listener:checkItems(p)
-	for itemID, status in next, p[self.types.item] do
-		if status == -1 then
-			local pt = p[self.types.item]
-
-			if C_Item.DoesItemExistByID(itemID) then
-				local isCached = C_Item.IsItemDataCachedByID(itemID)
-
-				if isCached then
-					pt[itemID] = isCached
-					pt.count = pt.count + 1
-				else
-					self:loadID(self.types.item, itemID, p)
-				end
-			else
-				pt[itemID] = false
-				pt.count = pt.count + 1
-			end
-		end
-	end
-end
-
-
-function listener:checkSpell(p)
-	for spellID, status in next, p[self.types.spell] do
-		if status == -1 then
-			local pt = p[self.types.spell]
-
-			if C_Spell.DoesSpellExist(spellID) then
-				local isCached = C_Spell.IsSpellDataCached(spellID)
-
-				if isCached then
-					pt[spellID] = isCached
-					pt.count = pt.count + 1
-				else
-					self:loadID(self.types.spell, spellID, p)
-				end
-			else
-				pt[spellID] = false
-				pt.count = pt.count + 1
-			end
-		end
-	end
-end
-
-
-if listener.types.quest then
-	function listener:checkQuests(p)
-		for questID, status in next, p[self.types.quest] do
-			if status == -1 then
-				self:loadID(self.types.quest, questID, p)
-			end
-		end
-	end
-end
-
-
 function listener:fill(loadType, p, ids, ...)
-	local t = {}
+	local t = p[loadType] or {}
 	p[loadType] = t
 
 	if type(ids) == "number" then ids = {ids, ...} end
 
 	if type(ids) == "table" then
-		t.count = 0
-		t.total = #ids
+		t.count = t.count or 0
+		t.total = t.total or 0
 
-		for i = 1, t.total do
-			t[ids[i]] = -1
+		for i = 1, #ids do
+			if t[ids[i]] == nil then
+				t[ids[i]] = -2
+				t.total = t.total + 1
+			end
 		end
+
+		return ids, t
 	else
 		error("Bad arguments (table of IDs or IDs expected)")
 	end
@@ -230,36 +176,74 @@ end
 
 
 function methods:AddItems(...)
-	if not self[listener.types.item] then
-		listener:fill(listener.types.item, self, ...)
-		listener:checkItems(self)
-		return self
-	else
-		error("Items table already exists")
+	local loadType = listener.types.item
+	local t, pt = listener:fill(loadType, self, ...)
+
+	for i = 1, #t do
+		local itemID = t[i]
+
+		if pt[itemID] == -2 then
+			if DoesItemExistByID(itemID) then
+				if IsItemDataCachedByID(itemID) then
+					pt[itemID] = true
+					pt.count = pt.count + 1
+				else
+					pt[itemID] = -1
+					listener:loadID(loadType, itemID, self)
+				end
+			else
+				pt[itemID] = false
+				pt.count = pt.count + 1
+			end
+		end
 	end
+
+	return self
 end
 
 
 function methods:AddSpells(...)
-	if not self[listener.types.spell] then
-		listener:fill(listener.types.spell, self, ...)
-		listener:checkSpell(self)
-		return self
-	else
-		error("Spells table already exists")
+	local loadType = listener.types.spell
+	local t, pt = listener:fill(loadType, self, ...)
+
+	for i = 1, #t do
+		local spellID = t[i]
+
+		if pt[spellID] == -2 then
+			if DoesSpellExist(spellID) then
+				if IsSpellDataCached(spellID) then
+					pt[spellID] = true
+					pt.count = pt.count + 1
+				else
+					pt[spellID] = -1
+					listener:loadID(loadType, spellID, self)
+				end
+			else
+				pt[spellID] = false
+				pt.count = pt.count + 1
+			end
+		end
 	end
+
+	return self
 end
 
 
 if listener.types.quest then
 	function methods:AddQuests(...)
-		if not self[listener.types.quest] then
-			listener:fill(listener.types.quest, self, ...)
-			listener:checkQuests(self)
-			return self
-		else
-			error("Quests table already exists")
+		local loadType = listener.types.quest
+		local t, pt = listener:fill(loadType, self, ...)
+
+		for i = 1, #t do
+			local questID = t[i]
+
+			if pt[questID] == -2 then
+				pt[questID] = -1
+				listener:loadID(loadType, questID, self)
+			end
 		end
+
+		return self
 	end
 end
 
